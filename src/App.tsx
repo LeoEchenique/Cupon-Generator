9cdcf1316386e42bc01f05eefed30f8aa38eed2e
LeoEchenique <leo.echeni@gmail.com>
2023-02-04 17:40:17 -0300

    App now is login page: auth enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,66 @@
-import * as XLSX from "xlsx";
-import Nav from "./components/Nav";
-import "./App.css";
-import { useEffect, useState } from "react";
-import Cupon from "./components/Cupons";
-import "./index.css";
-import Swal from "sweetalert2";
+import "./appStyles/App.css";
+import { useState , useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import "./appStyles/index.css";
+import {checkUser, auth, } from "./firebase-config.js";
+
 function App() {
-  interface Icupon {
-    nombre: string;
-    primerVencimiento: number | string;
-    segundoVencimiento: number | string;
-    tercerVencimiento: number | string;
+
+  interface User{
+    username: string,
+    password: string
+  };
+
+const navigate= useNavigate();
+
+const [error,setError]= useState("");
+const [user,setUser]=useState({
+  username:"",
+  password:""
+});
+
+useEffect(()=>{
+  auth.signOut();
+},[])
+
+ const logUser= async ( user: User)=>{
+ let password= user.password;
+ let username= user.username;   
+  const access= await checkUser(auth, username, password); 
+  if(Array.isArray(access)) return setError("text-red-400") // if includes, means that there's some errors that needs to be handle by a controller form and display error/colors on the component
+ 
+  navigate(`inicio/${access}`)
   }
-  const [data, setData] = useState<Icupon[]>([]);
-  const [cupons, setCupons] = useState<Icupon[]>([]);
 
-  const normalize = (ws: any) => {
-    ws.A1 = {
-      ...ws.A1,
-      w: "Nombre",
-    };
-    ws.B1 = {
-      ...ws.B1,
-      w: "venc1",
-    };
-    ws.C1 = {
-      ...ws.C1,
-      w: "venc2",
-    };
-    ws.D1 = {
-      ...ws.D1,
-      w: "venc3",
-    };
-    return ws;
-  };
+const handleChange= (e:  React.ChangeEvent<HTMLInputElement>)=>{
+  setUser((prev)=>({
+    ...prev,
+    [e.target.name]: e.target.value
+  }));
+}
 
-  const readExcel = (file: any) => {
-    if (file.name.includes("xlsx") || file.type.includes("xml")) {
-      const fileReaded: any = new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.readAsArrayBuffer(file);
-        fileReader.onload = (e: any) => {
-          const bufferArray: any = e.target.result;
-          const wb = XLSX.read(bufferArray, { type: "buffer" });
-          const wsName = wb.SheetNames[0];
-          const ws = wb.Sheets[wsName];
-          const newKeys = normalize(ws);
-          const data = XLSX.utils.sheet_to_json(newKeys);
-          resolve(data);
-        };
-        fileReader.onerror = (err) => reject(err);
-      });
-      fileReaded.then((data: []) => {
-        let cupones: Icupon[] = data.map((e: any) => {
-          let obj: Icupon = {
-            nombre: e.Nombre,
-            primerVencimiento: e.venc1,
-            segundoVencimiento: e.venc2,
-            tercerVencimiento: e.venc3,
-          };
-          return obj;
-        });
-        setData(cupones);
-      });
-    } else {
-      Swal.fire("Ups! El archivo ingresado es inválido", undefined, "error")
-    }
-  };
-  useEffect(() => {
-    if (data.length) {
-      const editPrice = (str: string) => {
-        let slice = str.slice(0, 2).concat(".");
-        return slice.concat(str.slice(2));
-      };
-     let cuponsToDisplay: Icupon[] = data.map((e) => {
-       if(!e.primerVencimiento) e.primerVencimiento= 0;
-       if(!e.segundoVencimiento) e.segundoVencimiento=0;
-       if(!e.tercerVencimiento) e.tercerVencimiento=0;
-        let oneCuote = e.primerVencimiento.toString();
-        let twoCuote = e.segundoVencimiento.toString();
-        let threeCuote = e.tercerVencimiento.toString();
-        if (oneCuote.length === 5) {
-          return {
-            ...e,
-            primerVencimiento: editPrice(oneCuote),
-            segundoVencimiento: editPrice(twoCuote),
-            tercerVencimiento: editPrice(threeCuote),
-          };
-        }
-        return e;
-      });
-      setCupons(cuponsToDisplay); 
-    }
-  }, [data]);
   return (
-    <div className="App bg-zinc-800 w-[99vw]">
-      <Nav />
-      <div className=" h-[100vh] mt-12  flex flex-col items-center justify-start ">
-        <div className="h-2/4 mb-6 flex flex-col items-center justify-center" >
-          <div className="h-10 flex items-center justify-center">
-            <p className="text-white text-lg mb-12">Arrastra o busca tu archivo <b>EXCEL</b></p>
+    <div className="App bg-zinc-800 w-[99vw] ">
+    <Nav token={false}/>
+    <div className="h-[100vh] mt-12  flex flex-col items-center justify-start">
+       <div className="h-3/6 w-1/3 mt-16 max-md:mt-0  max-md:w-3/4 max-md:h-96 flex flex-col items-center justify-center gap-6 p-2 py-4 bg-zinc-600 border-4 text-white border rounded-lg">
+            <h1 className="text-2xl  mb-10">Inicia sesión para continuar</h1>
+          <div className=" mb-8 h-60 flex flex-col items-center justify-around">
+            <div className="flex flex-col gap-6">
+              <label htmlFor="username" >Usuario</label>
+             <input onChange={(e)=> handleChange(e)} type="text" name="username" className="border text-black border-gray-300 rounded-full h-10 pl-5 pr-10 bg-zinc-400  focus:outline-none" />
+            </div>
+            {error ? <p className={`${error}`}> Algunos datos ingresados son incorrectos </p> : null}
+            <div className="flex flex-col gap-6">
+              <label htmlFor="password" >Contraseña</label>
+              <input onChange={(e)=> handleChange(e)} type="password" name="password" className="border text-black border-gray-300 rounded-full h-10 pl-5 pr-10 bg-zinc-400  focus:outline-none" />
+            </div>
           </div>
-          <input
-            type="file"
-            className=" border-4 border-dotted border-gray-100 h-1/2  md: h-20 h-40 px-10 py-32 md:p-40 md:pt-1/2 text-white bg-zinc-600  "
-            onChange={(e: any) => readExcel(e.target.files[0])}
-            onDrop={(e:any)=> readExcel(e.target.files[0])}
-          />
-          
-        </div>
-        <div className="" >
-        <Cupon cupons={cupons} />
-        </div>
-        
-      
-        
+          <button className="w-1/3 bg-amber-800 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded-full " onClick={()=> logUser(user)}>Entrar</button>
+       </div>
        
-      </div>
+    </div>
     </div>
   );
 }
