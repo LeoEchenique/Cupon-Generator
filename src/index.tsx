5344a20438e14d79c67d05348085f1763a7dd29c
LeoEchenique <leo.echeni@gmail.com>
2023-02-04 17:39:57 -0300

    Index: change routes & auth enabled

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./appStyles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Nav from "./components/Nav";
+import About from "./components/about/About";
+import Dashboard from "./components/dashboard/Dashboard";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,15 +16,12 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={[ <Nav />,<About /> ]} />
+        <Route path= "/"  element={<App />} />
+        <Route path={`/inicio/:token`} element={<Dashboard />} />
+        <Route path="/about" element={[ <About key="5"/> ]} />
       </Routes>
-      
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
