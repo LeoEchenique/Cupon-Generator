3890de26bce9a1e82b7ee0a215ece34dea8ba39a
LeoEchenique <leo.echeni@gmail.com>
2023-02-03 23:08:13 -0300

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOGWIIKixpSJv2iUts5-RrcGJ3I0QncIY",
+  authDomain: "santa-ines-jard.firebaseapp.com",
+  projectId: "santa-ines-jard",
+  storageBucket: "santa-ines-jard.appspot.com",
+  messagingSenderId: "145381176859",
+  appId: "1:145381176859:web:f9b5c033031f9dd3c22abe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
