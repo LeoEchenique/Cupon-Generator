1dff3b1164dac76399c6796d78b12361a320897d
LeoEchenique <leo.echeni@gmail.com>
2023-02-07 14:43:12 -0300

    fix responsive H

--- src/App.tsx
+++ src/App.tsx
@@ -44,9 +44,10 @@ const handleChange= (e:  React.ChangeEvent<HTMLInputElement>)=>{
     <div className="App bg-zinc-800 w-[99vw] ">
     <Nav token={false}/>
     <div className="h-[100vh] mt-12  flex flex-col items-center justify-start">
-       <div className="md:min-h-min w-1/3 mt-16 max-md:mt-0  max-md:w-3/4 max-md:h-96 flex flex-col items-center justify-center gap-6 p-2 py-4 bg-zinc-600 border-4 text-white border rounded-lg">
+       <div className="md:min-h-min max-md:h-auto w-1/3 mt-16 max-md:mt-0  max-md:w-3/4 max-md:h-96 flex flex-col items-center justify-center gap-6 p-2 py-4 bg-zinc-600 border-4 text-white border rounded-lg">
             <h1 className="text-2xl  mb-10">Inicia sesión para continuar</h1>
-          <div className=" mb-8 h-60 flex flex-col items-center justify-around">
+          
+            <div className=" mb-8 h-60 flex flex-col items-center justify-around">
             <div className="flex flex-col gap-6">
               <label htmlFor="username" >Usuario</label>
              <input onChange={(e)=> handleChange(e)} type="text" name="username" className="border text-black border-gray-300 rounded-full h-10 pl-5 pr-10 bg-zinc-400  focus:outline-none" />
@@ -57,7 +58,7 @@ const handleChange= (e:  React.ChangeEvent<HTMLInputElement>)=>{
               <input onChange={(e)=> handleChange(e)} type="password" name="password" className="border text-black border-gray-300 rounded-full h-10 pl-5 pr-10 bg-zinc-400  focus:outline-none" />
             </div>
           </div>
-          <button className="w-1/3 bg-amber-800 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded-full " onClick={()=> logUser(user)}>Entrar</button>
+          <button type="submit" className="w-1/3 bg-amber-800 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded-full " onClick={()=> logUser(user)}>Entrar</button>
        </div>
        
     </div>
